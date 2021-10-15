4b7cec2601bfe28547517eb548dbbc5f7b687a27
Omar-alrantisi <omaratef.alrantisi@gmail.com>
2021-10-15 16:58:16 +0300

    add JS to my Calculater

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+    // console.log("cc");
+   }
+   
+   
+   function display(value) {
+    document.getElementById("result").value += value;
+   }
+  
+   function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+    
+   }
+
+  
